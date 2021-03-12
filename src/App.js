fd783c3c94a769db32f5b2609bdb1a656ca34887
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-12 17:49:59 +0600

    single user

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
   useEffect(() => {
     const url = 'https://jsonplaceholder.typicode.com/users';
     fetch(url)
     .then(res => res.json())
     .then(data => setUsers(data))
+
+    // singleUser
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUser(data))
   }, [])
   return (
     <div>
@@ -15,6 +21,9 @@ function App() {
     {
       users.map(user => <li>{user.name}</li>)
     }
+
+    <h1>Single User Name: {singleUser.name}</h1>
+
     </div>
   );
 };
