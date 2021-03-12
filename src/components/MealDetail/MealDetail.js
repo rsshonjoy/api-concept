f7abe49a75f963471ff413c22ac8644a5ba2157b
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-12 19:26:43 +0600

    perfect away

--- src/components/MealDetail/MealDetail.js
+++ src/components/MealDetail/MealDetail.js
@@ -7,13 +7,15 @@ const MealDetail = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`;
     fetch(url)
     .then(res => res.json())
-    .then(data => setMeal(data))
+    // .then(data => setMeal(data))
+    .then(data => setMeal(data.meals[0]))
   },[])
   return (
     <div>
       <h1>This is Meal Detail</h1>
       {/* <h2>Meal Name: {meal?.meals[0].strMeal}</h2> */}
-      <h2>Meal Name: {meal.meals?.[0].strMeal}</h2>
+      {/* <h2>Meal Name: {meal.meals?.[0].strMeal}</h2> */}
+      <h2>Meal Name: {meal.strMeal}</h2>
     </div>
   );
 };
