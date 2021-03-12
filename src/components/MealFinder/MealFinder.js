69f0d88f72e0ce186058d5f7eb5fcfd7bc691624
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-12 19:46:08 +0600

    search value show display

--- src/components/MealFinder/MealFinder.js
+++ src/components/MealFinder/MealFinder.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MealFinder = () => {
+  const [search, setSearch] = useState('');
   const handleChange = event => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setSearch(event.target.value);
   }
   return (
     <div>
       <h1>Find Delicious Foods</h1>
       <input type="text" onChange={handleChange} placeholder="search food"/>
+      <p>searching: {search}</p>
     </div>
   );
 };
