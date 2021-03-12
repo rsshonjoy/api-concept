cc27c95a03db8c68e8c95cf08bf267e0e094d7af
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-12 19:22:18 +0600

    optional chaining 2

--- src/components/MealDetail/MealDetail.js
+++ src/components/MealDetail/MealDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 const MealDetail = () => {
-  const [meal, setMeal] = useState();
+  // const [meal, setMeal] = useState();
+  const [meal, setMeal] = useState({});
   useEffect(() => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`;
     fetch(url)
@@ -11,7 +12,8 @@ const MealDetail = () => {
   return (
     <div>
       <h1>This is Meal Detail</h1>
-      <h2>Meal Name: {meal?.meals[0].strMeal}</h2>
+      {/* <h2>Meal Name: {meal?.meals[0].strMeal}</h2> */}
+      <h2>Meal Name: {meal.meals?.[0].strMeal}</h2>
     </div>
   );
 };
