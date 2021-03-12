d3649d59b0bd84b4d95098b44faf8f77490af044
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-12 17:32:48 +0600

    JSON Placeholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>JSON Placeholder</h1>
+    {
+      users.map(user => <li>{user.name}</li>)
+    }
     </div>
   );
-}
+};
 
 export default App;
